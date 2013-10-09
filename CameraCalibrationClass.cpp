62e74d936b9d02227861f0ead27d944113a56a68
Lilita Kiforenko <lilita@kiforenko.eu>
2013-10-09 19:40:30 +0200

    load files from folder

--- CameraCalibrationClass.cpp
+++ CameraCalibrationClass.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "CameraCalibrationClass.h"
-
+#include <stdio.h>
 
 CameraCalibrationClass::~CameraCalibrationClass() {
 	// TODO Auto-generated destructor stub
@@ -31,16 +31,30 @@ int CameraCalibrationClass::addChessboardPoints(const std::vector<std::string>& 
 	}
 	cv::Mat image;
 	int successes = 0;
-	for (int i=0; i< filelist.size(); i++)
+	for (unsigned int i=0; i< filelist.size(); i++)
 	{
 		image = cv::imread(filelist[i],0);
+
 		bool found = cv::findChessboardCorners(image, boardSize, imageCorners);
+//		if (i == 100)
+//		{
+//			drawChessboardCorners(image, boardSize, imageCorners, found);
+//			//imshow("corners", image);
+//			imwrite("corners.png", image);
+//			waitKey(0);
+//		}
+
 		cv::cornerSubPix(image, imageCorners, cv::Size(5,5), cv::Size(-1,-1),
 				cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 30, 0.1));
 		if (imageCorners.size() == boardSize.area())
 		{
 			addPoints(imageCorners, objectCorners);
 			successes++;
+		}
+		else
+		{
+			cout << i << ": " << filelist[i] << endl;
+			remove(filelist[i].c_str());
 		}
 	}
 	return successes;
@@ -51,7 +65,7 @@ double CameraCalibrationClass::calibrate(cv::Size &imageSize)
 	mustInitUndistort= true;
 	std::vector<cv::Mat> rvecs, tvecs;
 	double res = 	calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs, flag);
-    cout  << cameraMatrix << endl;
+	cout  << cameraMatrix << endl;
 
 	return res;
 
--- cal.cpp
+++ cal.cpp
@@ -3,30 +3,48 @@
 #include <cstdio>
 #include "CameraCalibrationClass.h"
 
+#include <dirent.h>
 
 using namespace cv;
 using namespace std;
 
 int main()
 {
-	char path[255] = "/home/lulu/cameraCalibration/frame";
-	char dest[255];
+	char path[255] = "/home/lulu/cameraCalibration/";
+	unsigned int sample_size = 300;
 	Size boardSize(9, 6);
 	vector<string> imagePath;
-	for (int i = 1; i < 15; i++ )
-	{
-		sprintf(dest, "%s%d.tiff", path, i);
-		cout<< dest<< endl;
-		imagePath.push_back(string(dest));
-	}
+//	for (int i = 1; i < 15; i++ )
+//	{
+//		sprintf(dest, "%s%d.tiff", path, i);
+//		cout<< dest<< endl;
+//		imagePath.push_back(string(dest));
+//	}
 
+	DIR *dir;
+	class dirent *ent;
+
+	dir = opendir(path);
+	string full_path;
+	while ((ent = readdir(dir)) != NULL) {
+		if(ent->d_type == DT_REG)
+		{
+			full_path = string(path);
+			full_path.append(ent->d_name);
+
+			imagePath.push_back(full_path);
+
+			if(sample_size <= 0)
+				break;
+
+			--sample_size;
+		}
+	}
 
 	CameraCalibrationClass cc = CameraCalibrationClass();
 	int s  = cc.addChessboardPoints(imagePath, boardSize);
 	cout << s << endl;
 	double res = cc.calibrate(boardSize);
-
-
 
 	cout << "Final re-proj error: "<< res << endl;
 	waitKey(0);
